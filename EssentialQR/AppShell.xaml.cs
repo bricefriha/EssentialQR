b6b353444ac656805d2fddf21658865114076924
Brice Friha <Brice.friha@outlook.com>
2023-10-27 20:47:12 +0100

    Add project files.

---
+++ EssentialQR/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EssentialQR
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EssentialQR/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EssentialQR
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
